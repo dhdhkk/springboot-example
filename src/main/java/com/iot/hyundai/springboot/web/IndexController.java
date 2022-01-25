6591ce625657416418f9a790eff998270c492dae
kwon donghyun <dhdhkk3@gmail.com>
2022-01-25 13:39:11 +0900

    7장까지 완료

--- src/main/java/com/iot/hyundai/springboot/web/IndexController.java
+++ src/main/java/com/iot/hyundai/springboot/web/IndexController.java
@@ -1,5 +1,7 @@
 package com.iot.hyundai.springboot.web;
 
+import com.iot.hyundai.springboot.config.auth.LoginUser;
+import com.iot.hyundai.springboot.config.auth.dto.SessionUser;
 import com.iot.hyundai.springboot.service.posts.PostsService;
 import com.iot.hyundai.springboot.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
@@ -9,6 +11,8 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
@@ -16,9 +20,12 @@ public class IndexController {
     private final PostsService postsService;
 
     @GetMapping("/")
-    public String index(Model model)
-    {
+    public String index(Model model, @LoginUser SessionUser user) {
         model.addAttribute("posts", postsService.findAllDesc());
+
+        if (user != null) {
+            model.addAttribute("userName", user.getName());
+        }
         return "index";
     }
 
