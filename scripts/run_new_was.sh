6eb6dd7fb9e5fd5ed9d8f7a9a7feedccb5b1a9bc
kwon donghyun <dhdhkk3@gmail.com>
2022-01-28 14:58:43 +0900

    [FEAT] run_new_was.sh -jar 문법 오류 수정

--- scripts/run_new_was.sh
+++ scripts/run_new_was.sh
@@ -30,7 +30,7 @@ fi
 #exit 0
 
 nohup java -jar \
-        -Dserver.port=${TARGET_PORT}
+        -Dserver.port=${TARGET_PORT} \
         -Dspring.config.location=classpath:/application.properties,classpath:/application-real.properties,/home/ec2-user/app/application-oauth.properties,/home/ec2-user/app/application-real-db.properties \
         -Dspring.profiles.active=real \
         $REPOSITORY/zip/build/libs/* > /home/ec2-user/app/step2/nohup.out 2>&1 &
