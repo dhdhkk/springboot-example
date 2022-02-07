cd4cb35d8658f507ec5e1d1b17e2883035410a8a
kwon donghyun <dhdhkk3@gmail.com>
2022-02-07 14:08:54 +0900

    [TEST] 무중단 배포 테스트9

--- scripts/run_new_was.sh
+++ scripts/run_new_was.sh
@@ -36,6 +36,6 @@ fi
 #        -Dspring.profiles.active=real \
 #        /home/ec2-user/app/step2/zip/build/libs/* > /home/ec2-user/app/step2/nohup.out 2>&1 &
 
-nohup java -jar -Dserver.port=${TARGET_PORT} -Dspring.config.location=classpath:/application.properties,classpath:/application-real.properties,/home/ec2-user/app/application-oauth.properties,/home/ec2-user/app/application-real-db.properties /home/ec2-user/app/step2/zip/build/libs/* > /home/ec2-user/app/step2/nohup.out 2>&1 &
+nohup java -jar -Dserver.port=${TARGET_PORT} -Dspring.config.location=classpath:/application.properties,classpath:/application-real.properties,/home/ec2-user/app/application-oauth.properties,/home/ec2-user/app/application-real-db.properties -Dspring.profiles.active=real /home/ec2-user/app/step2/zip/build/libs/* > /home/ec2-user/app/step2/nohup.out 2>&1 &
 echo "> Now new WAS runs at ${TARGET_PORT}."
 exit 0
